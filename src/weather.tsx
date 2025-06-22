9105c529cc079b45b93fed01f6efb6b04c3d63c7
Nikhil <123183868+0xNikhilCodes@users.noreply.github.com>
2025-06-22 22:18:29 +0530

    Update weather.tsx

--- src/weather.tsx
+++ src/weather.tsx
@@ -1,25 +1,48 @@
 import React, { useEffect, useState } from "react";
 
+interface WeatherData {
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: {
+    main: string;
+  }[];
+  wind: {
+    speed: number;
+  };
+}
+
 const Weather = () => {
-  const [weather, setWeather] = useState(null);
-  const [city,setcity]=useState('');
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [city, setcity] = useState("");
 
   const API_KEY = "38b1fb8e326f9703416f317dfddb3cf0";
-    function getdata(){
 
+  function getdata() {
     const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(URL)
-      .then(res => res.json())
-      .then(data => setWeather(data));
+      .then((res) => res.json())
+      .then((data) => setWeather(data));
   }
 
-
   return (
-    <div style={{ textAlign: "center", padding: "30px",color:"black" }}>
+    <div style={{ textAlign: "center", padding: "30px", color: "black" }}>
       <h1>🌤️ Weather App</h1>
-      <input type="text" value={city} placeholder="Enter the city name" onChange={e=>{setcity(e.target.value)}} style={{padding:'5px',background:"black",borderRadius:"2px"}}/>
-      <button onClick={getdata} style={{margin:"5px"}}>GetData</button>
-       {weather ? (
+      <input
+        type="text"
+        value={city}
+        placeholder="Enter the city name"
+        onChange={(e) => {
+          setcity(e.target.value);
+        }}
+        style={{ padding: "5px", background: "black", borderRadius: "2px" }}
+      />
+      <button onClick={getdata} style={{ margin: "5px" }}>
+        GetData
+      </button>
+      {weather ? (
         <div>
           <h2>{weather.name}</h2>
           <p>🌡️ Temp: {weather.main.temp}°C</p>
